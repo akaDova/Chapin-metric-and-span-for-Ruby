9a3dea618d0689bdb1f1b48261e7450820f341b9
Nikitos K <miraak.dova@gmail.com>
2017-11-13 21:32:49 +0300

    Update and rename spen.js to span.js

--- span.js
+++ span.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
 
-function Spen() {
+function Span() {
   var text, idents;
     (function init() {
-        text = fs.readFileSync('./parse.rb', 'utf-8');
+        text = fs.readFileSync('./code.rb', 'utf-8');
         idents = text.replace(/;/g,"\n").split(/\n/)
             .filter(function(str){ return str != "" && /\s+=\s+.+/.test(str) })
             .map(str => str.trim().replace(/\s+=\s+.+/, ""))
@@ -17,10 +17,10 @@ function Spen() {
   this.idents = idents;
 }
 
-Spen.prototype.show = function() {
+Span.prototype.show = function() {
     console.log(this.idents.map(elem => `${elem.id}: ${elem.num}`).join("\n") + '\nsum: ' + this.idents.map(elem => elem.num)
-.reduce((prev, curr) => prev + curr, 0));
+                .reduce((prev, curr) => prev + curr, 0));
 
 };
 
-module.exports = Spen;
+module.exports = Span;
